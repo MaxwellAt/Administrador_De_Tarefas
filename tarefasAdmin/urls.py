419ed4ddeab292198f61d2eee32a814dba159587
Jêfferson Maxwell <jeffersonmaxwell1603@gmail.com>
2023-04-14 17:01:29 -0300

    New features, like be: create, add, remove or update

--- tarefasAdmin/urls.py
+++ tarefasAdmin/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('AdminTarefas/', include('AdminTarefas.urls'))
+    path('', include('AdminTarefas.urls'))
 ]
